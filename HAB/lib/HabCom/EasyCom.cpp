0c909f6b786f2517ba5f542383944e4cc8f9f0d0
bediak <35378640@users.noreply.github.com>
2018-09-30 12:52:25 +0200

    upraveno pro volitelne pouziti HW nebo SW serialu

--- HAB/lib/HabCom/EasyCom.cpp
+++ HAB/lib/HabCom/EasyCom.cpp
@@ -29,21 +29,42 @@ void EasyCom::setSwPins(int RX, int TX) {
 }
 
 bool EasyCom::begin() {
-  Serial.begin(_speed);
+  if (_HwSerialUsed) {
+    Serial.begin(_speed);
+  }
+  else {
+    EasyCom::_COM.setPins(_RX, _TX);
+    EasyCom::_COM.begin(_speed);
+  }
   return true;
 }
 
 int EasyCom::available() {
-  return Serial.available();
+  if (_HwSerialUsed)
+    return Serial.available();
+  else
+    return EasyCom::_COM.available();
+
+  return 0; // sem uz by se program nikdy nemel dostat, je to jen kvuli kompilatoru
 }
 
 int EasyCom::read() {
-  int data = Serial.read();
+  int data = -1;
+  if (_HwSerialUsed)
+    data = Serial.read();
+  else
+    data = EasyCom::_COM.read();
+
   return data;
 }
 
 byte EasyCom::write(const byte what) {
-  return Serial.write(what);
+  if (_HwSerialUsed)
+    return Serial.write(what);
+  else
+    return EasyCom::_COM.write(what);
+  
+  return 0; // sem uz by se program nikdy nemel dostat, je to jen kvuli kompilatoru
 }
 
 void EasyCom::blink(byte count /* = 3 */, int duration /* = 200 */) {
@@ -56,5 +77,8 @@ void EasyCom::blink(byte count /* = 3 */, int duration /* = 200 */) {
 }
 
 void EasyCom::println(const int number) {
-  Serial.println(number);
+  if (_HwSerialUsed)
+    Serial.println(number);
+  else
+    EasyCom::_COM.println(number);
 }
